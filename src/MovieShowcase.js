b74d7740ce0c3855a525f66b1ad4cd181124ac60
github username <github email address>
2019-09-16 12:21:07 -0400

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -5,6 +5,9 @@ import movieData from './data.js'
 export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
+    return movieData.map((attr, index) => 
+      <MovieCard key={index} {...attr} />
+    )
     // map over your movieData array and return the correct 
   }
 
